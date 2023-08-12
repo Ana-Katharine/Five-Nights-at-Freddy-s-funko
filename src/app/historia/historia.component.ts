c5002f1713cce9d7c200ce5cd92c50b70dfeb6da
Ana Karina <ana_silvaleitao@outlook.com.br>
2023-08-12 16:47:53 -0300

    Atualizando história - Freddy

--- src/app/historia/historia.component.ts
+++ src/app/historia/historia.component.ts
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
 export class HistoriaComponent {
   titulo: string = 'Freddy';
   foto1: string = 'https://i.ibb.co/hKJB16w/freddy-removebg-preview.png';
-  foto2: string = 'https://i.ibb.co/LJPBv6S/Idle-Freddy.webp';
+  foto2: string = 'https://i.ibb.co/rZFMK1t/Idle-WFreddy.webp';
 
   description1: string = 'Freddy Fazbear, o carismático urso animatrônico, uma vez foi a estrela do restaurante Freddy Fazbear s Pizza, encantando as crianças com suas canções alegres e danças.';
   description2: string = 'No entanto, à noite, ele e seus amigos ganham vida de maneira apavorante, perseguindo qualquer um que se aventurasse no restaurante.';
